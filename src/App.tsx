d1d16d0f82f746c092675d936c1973389a29c857
Adithya <aadithyaavardhan@gmail.com>
2024-06-16 18:10:29 +0530

    form validation in typescript and zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { createApi } from "unsplash-js";
+import { useState } from "react";
+import { ChangeEvent, FormEvent } from "react";
+import { z } from "zod";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const invalid_type_error: string = "field should be filled";
+
+  const Note = z.object({
+    title: z
+      .string({ invalid_type_error })
+      .min(3, { message: "title should be more than 3 characters" }),
+    description: z
+      .string({ invalid_type_error })
+      .min(3, { message: "title should be more than 3 characters" }),
+    tag: z
+      .string({ invalid_type_error })
+      .min(3, { message: "title should be more than 3 characters" }),
+  });
+
+  interface Notes {
+    title: string;
+    description: string;
+    tag: string;
+  }
+
+  const [notes, setNotes] = useState<Notes>({
+    title: "",
+    description: "",
+    tag: "",
+  });
+
+  const handelChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNotes({
+      ...notes,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    try {
+      const res = Note.parse(data);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Welcome to the note taking app </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Tile</label>
+        <input type="text" name="title" onChange={handelChange} id="title" />
+        <label htmlFor="description">description</label>
+        <input
+          type="text"
+          name="description"
+          onChange={handelChange}
+          id="description"
+        />
+        <label htmlFor="tag">Tag</label>
+        <input type="text" name="tag" onChange={handelChange} id="tag" />
+        <button type="submit">submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
